73ca04c9700aa2f2494c8894afe4713b1be16487
yuryqwer <yuryqwer@outlook.com>
2023-09-13 23:35:26 +0800

    implement merge sort

--- sort/sort.go
+++ sort/sort.go
@@ -1,6 +1,9 @@
 package sort
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func InsertionSort(data sort.Interface) {
 	n := data.Len()
@@ -15,5 +18,36 @@ func InsertionSort(data sort.Interface) {
 			i -= 1
 			j -= 1
 		}
+	}
+}
+
+func merge(data []int, p, q, r int) {
+	left := make([]int, q-p+2)
+	right := make([]int, r-q+1)
+	copy(left, data[p:q+1])
+	copy(right, data[q+1:r+1])
+	left[len(left)-1] = int(math.Inf(1))
+	right[len(right)-1] = int(math.Inf(1))
+
+	leftIndex := 0
+	rightIndex := 0
+
+	for i := p; i < r+1; i++ {
+		if left[leftIndex] < right[rightIndex] {
+			data[i] = left[leftIndex]
+			leftIndex += 1
+		} else {
+			data[i] = right[rightIndex]
+			rightIndex += 1
+		}
+	}
+}
+
+func MergeSort(data []int, p, r int) {
+	if p < r {
+		q := (p + r) / 2
+		MergeSort(data, p, q)
+		MergeSort(data, q+1, r)
+		merge(data, p, q, r)
 	}
 }
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -10,3 +10,9 @@ func TestInsertionSort(t *testing.T) {
 	InsertionSort(a)
 	t.Error(a)
 }
+
+func TestMerge(t *testing.T) {
+	a := []int{8, 4, 2, 5, 7, 9, 3, 2, 6, 1}
+	MergeSort(a, 0, len(a)-1)
+	t.Error(a)
+}
